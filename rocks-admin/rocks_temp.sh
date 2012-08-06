d96d69b6474a9957aadd199ba688cfe0245edfad
Alejandro Gomez <alegomez544@gmail.com>
2012-08-06 06:57:45 -0500

    added comments to the script

--- rocks-admin/rocks_temp.sh
+++ rocks-admin/rocks_temp.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+#array for storing the data
+
 declare -a TEMP
+
+#get and parse the data
 
 TEMP=`rocks run host "ipmitool sdr type Temperature" | grep CPU | grep degrees | sed -e 's/.* \([0-9]*\) degrees C$/\1/'`
 
+#some useful variables for the average calculus.
+
 TOTAL_TMP=0
 NUM=0
+
+#you know what this loop does, aren't you?
 
 for tmp in ${TEMP[*]}
 do
@@ -13,8 +21,14 @@ do
     NUM=`expr $NUM + 1`
 done
 
+#as you can expect, this calculate the average 
+#of the temperature on every node in the cluster
+
 TOTAL_TMP=`expr $TOTAL_TMP / $NUM`
 
 
-echo "la temperatura promedio de $NUM procesadores es":
-echo "$TOTAL_TMP Grados Centigrados"
+
+echo "the average temperature of $NUM cores is":
+echo "$TOTAL_TMP C degrees"
+
+#just like magic, isn't it?
